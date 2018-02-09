dec740cd4e0725584f99d336fdc0b0357c274923
Artemis <zer00aeon@gmail.com>
2018-02-09 15:21:25 +0200

    Playing with multiprocess

--- parallel_execution/parallel.py
+++ parallel_execution/parallel.py
@@ -21,26 +21,45 @@ scientists = {
     Scientist(name='Tata Borbus', field='Social', born=1979, nobel=True),
     Scientist(name='Janna Borbus', field='N/A', born=1979, nobel=True),
     Scientist(name='Vova Borbus', field='Medicine', born=1979, nobel=True),
-    Scientist(name='Troy Borbus', field='Happy', born=1979, nobel=True)
+    Scientist(name='Troy Borbus', field='Happy', born=1979, nobel=True),
+    Scientist(name='Vova Borbus', field='Medicine', born=1979, nobel=True),
+    Scientist(name='AVova Borbus', field='Medicine', born=1979, nobel=True),
+    Scientist(name='BVova Borbus', field='Medicine', born=1979, nobel=True),
+    Scientist(name='CVova Borbus', field='Medicine', born=1979, nobel=True),
+    Scientist(name='DVova Borbus', field='Medicine', born=1979, nobel=True),
+    Scientist(name='FVova Borbus', field='Medicine', born=1979, nobel=True),
+    Scientist(name='EVova Borbus', field='Medicine', born=1979, nobel=True),
+    Scientist(name='WVova Borbus', field='Medicine', born=1979, nobel=True),
+    Scientist(name='QVova Borbus', field='Medicine', born=1979, nobel=True),
+    Scientist(name='RVova Borbus', field='Medicine', born=1979, nobel=True),
+    Scientist(name='RRVova Borbus', field='Medicine', born=1979, nobel=True),
+    Scientist(name='ZVova Borbus', field='Medicine', born=1979, nobel=True)
 }
 
 
 def transform(x):
     print(f'Processing record {x.name}' + "process running job is : " f'{os.getpid()}')
     time.sleep(1)
-    result = {'name': x.name, 'age': 2017 - x.born}
+    transformation_result = {'name': x.name, 'age': 2017 - x.born}
     print(f'Done processing record {x.name}')
-    return result
+    return transformation_result
 
 
 if __name__ == "__main__":
     pprint(scientists)
     start = time.time()
-    pool = multiprocessing.Pool()
+    # decide later which one is better
+    # pool = multiprocessing.Pool() # python is making decision regarding number of processes based on CPU spec?
+    # so far this is the faster variant on a single file
+    pool = multiprocessing.Pool(processes=len(scientists))  # I define separate process for each line
+
+    # pool = multiprocessing.Pool(processes=int(len(scientists) / 4),
+    #                             maxtasksperchild=1)  # I define to restart process once it is complete
+
     result = pool.map(transform, scientists)
     end = time.time()
     print(f'Time to complete: {end - start:.2f}')
-    pprint(result)
+    # pprint(result)
 
     print("Now single_process for comparison \n")
     start = time.time()
@@ -51,4 +70,3 @@ if __name__ == "__main__":
     ))
     end = time.time()
     print(f'Time to complete: {end - start:.2f}')
-    pprint(result)
