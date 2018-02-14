66e24defbb1cd22a01cd6f363472a9104446d8c6
smiroshn <smiroshn@akamai.com>
2018-02-14 17:14:03 +0200

    i love python , solving riddles

--- linxacad/landt.py
+++ linxacad/landt.py
@@ -1,4 +1,4 @@
-#solving python riddles just because i love pyton
+# solving python riddles just because i love pyton
 ml = [1, 2, 3, 4, 5, 6, 7, 8, "sdf", [1, 2, 3, 4, 5]]
 
 for item in ml:
@@ -16,7 +16,8 @@ print(t)
 print(reverse_t)
 polindrom_list = t + reverse_t
 
-place = int(len(polindrom_list)/2)
-polindrom_list[place], polindrom_list[place-1] = ['max' ], ['max']
+place = int(len(polindrom_list) / 2)
 
-print(polindrom_list )
+[polindrom_list[place], polindrom_list[place - 1]] = ["0000" for x in [polindrom_list] if len(polindrom_list) % 2 == 0], 11111
+
+print(polindrom_list)
