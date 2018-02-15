04f77c5c2439ec0506c93473e60142fb855b08b5
Artemis <zer00aeon@gmail.com>
2018-02-15 19:52:25 +0200

    refactor for better user classification

--- linxacad/ex2.py
+++ linxacad/ex2.py
@@ -1,21 +1,24 @@
 smiroshnikov = {'admin': True, 'active': True, 'name': "Morbusborbus"}
 tmiroshnikov = {'admin': False, 'active': True, 'name': "Dinosavri4ek"}
 jmiroshnikov = {'admin': False, 'active': False, 'name': "Rybka"}
+ymiroshnikov = {'admin': False, 'active': True, 'name': "Kroka"}
 
 
 def checkuser(user):
-    if user['admin']:
-        print(f"ADMIN {user['name']}")
-    if user['active']:
-        print(f"ACTIVE {user['name']}")
     if user['active'] and user['admin']:
-        print(f"ACTIVE - (ADMIN) {user['name']}")
+        return (f"ACTIVE - (ADMIN) {user['name']}")
+    elif user['admin']:
+        return (f"ADMIN {user['name']}")
+    if user['active']:
+        return (f"ACTIVE {user['name']}")
+
     elif (not user['active'] or not user['admin']):
-        print(f"username {user['name']}")
-    print("Done----------------------------\n")
+        return (f"username {user['name']}")
 
 
-users = (smiroshnikov, tmiroshnikov, jmiroshnikov)
-
+users = (smiroshnikov, tmiroshnikov, jmiroshnikov, ymiroshnikov)
+line = 1
 for user in users:
-    checkuser(user)
+    print(line ,":", checkuser(user))
+    # checkuser(user)
+    line += 1
