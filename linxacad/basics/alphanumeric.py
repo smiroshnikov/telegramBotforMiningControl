1a6699f1bf34011fac4cbd320cc10b71208a3379
smiroshn <smiroshn@akamai.com>
2018-02-20 19:06:47 +0200

    python way

---
+++ linxacad/basics/alphanumeric.py
@@ -0,0 +1,17 @@
+import string
+
+x = "ksdjf;lsjkdf 126786 slkj <<<>> 12§1  "
+
+for char in x:
+    if char not in string.ascii_letters:
+        print(char)
+
+# DONT DO THIS IN PYTHON, this is fucked up python , this is not python
+# spaces = 0
+# for c in x:
+#     if (' ' in x):
+#         print("space found ", spaces)
+#         spaces += 1
+
+
+print(x.count(" "))
--- linxacad/basics/bmi.py
+++ linxacad/basics/bmi.py
@@ -1,14 +1,34 @@
 #!bin/python
 
-height = float(input("Whats your height :? (inches or meters) "))
-weight = float(input("Whats your weight :? (pounds or kilograms) "))
-unit = input("Are your measurements in metric or imperial units ? ".lower().strip())
 
-if unit.startswith("i"):
-    bmi = 703 * (weight / (height ** 2))
-    print(bmi)
-elif unit.startswith("m"):
-    bmi = (weight / (height ** 2))
-    print(bmi)
-else:
-    print("unsupported measurements . fuck off")
+def get_user_data():
+    height = input("Whats your height :? (inches or meters) ")
+
+    weight = input("Whats your weight :? (pounds or kilograms) ")
+    unit = input("Are your measurements in metric or imperial units ? ").lower().strip()
+    return weight, height, unit
+
+
+def calculate_bmi(weight, height, unit="metric"):
+    if unit == "metric":
+        bmi = (float(weight / (height) ** 2))
+    else:
+        bmi = 703 * (float(weight / (height ** 2)))
+    return bmi
+
+
+while True:
+    height, weight, unit = get_user_data()
+
+    if (unit.lower().startswith("q") or
+            weight.lower().startswith("q") or
+            height.lower().startswith("q")):
+        print("Quiting ....")
+        break
+    elif unit.startswith("i"):
+        print(f"Your BMI is {calculate_bmi(height=height, weight=weight, unit='imperial')}")
+    elif unit.startswith("m"):
+        print(f"Your BMI is {calculate_bmi(height=height, weight=weight, unit='metric')}")
+
+    else:
+        print(f"Unsupported format {unit}, FU!")
